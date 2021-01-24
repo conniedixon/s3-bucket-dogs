81c164c59d1ea0bfabf2b8f424a95ddde921695f
connie <conniedixon106@gmail.com>
2021-01-24 14:10:38 +0000

    tested posting dog route

---
+++ BEN/spec/app.spec.js
@@ -0,0 +1,29 @@
+const chai = require("chai")
+const request = require("supertest")
+const chaiSorted = require("chai-sorted");
+const { expect } = chai.use(chaiSorted);
+const app = require("../app")
+
+describe("APP", () => {
+    it("returns an array of objects in s3", () => {
+        return request(app)
+        .get("/api/")
+        .expect(200)
+        .then(({body: {Contents}}) => {
+            expect(Contents).to.be.an("array")
+            expect(Contents[0]).to.be.an("object")
+        })
+    })
+    it("uploads a file to s3", () => {
+        return fs.readFile("models/testImage.jpg", async (err, data) => { 
+            return request(app)
+            .post("/api/")
+            .send(data)
+            .expect(200)
+            .then(({body}) => {
+                expect(body).to.be.an("object")
+                expect(body).to.contain.keys(["ETag", "Location", "Key", "Bucket"])
+            })
+        })
+    })
+})
